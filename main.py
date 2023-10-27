2238f6b3d8d492ead6b83f0c9638fbfa686a7e44
Moe Odeh <m.odeh93@gmail.com>
2023-10-26 22:57:13 -0400

    (Very) ugly but working colormap 3D plot of uncertainties

--- main.py
+++ main.py
@@ -56,39 +56,135 @@ C_max = C_3D.max( axis=0 ); C_min = C_3D.min( axis=0 )  # min/max across depth
 D_max = D_3D.max( axis=0 ); D_min = D_3D.min( axis=0 )  # min/max across depth
 
 # ======================================
-# --- 3D BAR COLORMAP PLOTS
+# --- 3D BAR COLORMAP PLOTS (A MATRIX)
 # ======================================
-import  matplotlib.pyplot               as  plt
-import  matplotlib.cm                   as  cm          # Import colormap stuff!
+import  matplotlib.pyplot               as      plt
+from    matplotlib                      import  colormaps # Import colormaps!
 
+# Find percent change between max and min
+def get_change( max_elem, min_elem):
+    if max_elem == min_elem:
+        return 0
+    try:
+        if(min_elem == 0): return 0
+        else: return (abs(max_elem - min_elem) / abs(min_elem)) * 100.0
+    except ZeroDivisionError:
+        # return float('inf')
+        return 0
+    
+delta_A = np.zeros_like( A_nom )
+for ith_Row, ith_Col in np.ndindex(A_nom.shape):
+    print( f'(ith_Row, ith_Col) = ({ith_Row}, {ith_Col})' )
+    max_elem = A_max[ith_Row, ith_Col]
+    min_elem = A_min[ith_Row, ith_Col]
+    delta_A[ith_Row, ith_Col] = get_change(max_elem, min_elem)
+
+# Normalize values
+A_final = delta_A / np.amax(delta_A)
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-x, y = np.random.rand(2, 100) * 4
-hist, xedges, yedges = np.histogram2d(x, y, bins=4, range=[[0, 4], [0, 4]])
+ax = plt.axes(projection = "3d")
+ 
+countries = [" ","Australia", " ","Brazil", " ","Canada", " ","France"]
 
-# Construct arrays for the anchor positions of the 16 bars.
-# Note: np.meshgrid gives arrays in (ny, nx) so we use 'F' to flatten xpos,
-# ypos in column-major order. For numpy >= 1.7, we could instead call meshgrid
-# with indexing='ij'.
-xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25)
-xpos = xpos.flatten('F')
-ypos = ypos.flatten('F')
-zpos = np.zeros_like(xpos)
+data = A_final.T
+years = [None, None, 2005, None, 2010, None, 2015]
+ 
+numOfRows = data.shape[0]
+numOfCols = data.shape[1]
+ 
+xpos = np.arange(0, numOfCols, 1)
+ypos = np.arange(0, numOfRows, 1)
+xpos, ypos = np.meshgrid(xpos + 0.5, ypos + 0.5)
+ 
+xpos = xpos.flatten()
+ypos = ypos.flatten()
+zpos = np.zeros(numOfCols * numOfRows)
+ 
+dx = np.ones(numOfRows * numOfCols) * 0.5
+dy = np.ones(numOfCols * numOfRows) * 0.5
+dz = data.flatten()
 
-# Construct arrays with the dimensions for the 16 bars.
-dx = 0.5 * np.ones_like(zpos)
-dy = dx.copy()
-dz = hist.flatten()
+# --- Color map stuff
+# cmap = cm.get_cmap('jet') # Get desired colormap
+cmap = colormaps['jet'] # Get desired colormap
+max_height = np.max(dz)   # get range of colorbars
+min_height = np.min(dz)
+# scale each z to [0,1], and get their rgb values
+rgba = [cmap((k-min_height)/max_height) for k in dz]
+ax.set_xticks( [i+1 for i in range(0, numOfCols)],
+               [ '$x_1$', '$x_2$', '$x_3$', '$x_4$',
+                 '$x_5$', '$x_6$', '$x_7$', '$x_8$',
+                 '$x_9$', '$x_{10}$', '$x_{11}$' ] )
+ax.set_yticks( [i+1 for i in range(0, numOfRows)],
+               [ '$x_1$', '$x_2$', '$x_3$', '$x_4$',
+                 '$x_5$', '$x_6$', '$x_7$', '$x_8$',
+                 '$x_9$', '$x_{10}$', '$x_{11}$' ] )
+ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=rgba, zsort='average')
+# ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=rgba )
+# --- END
+ 
+# ax.bar3d(xpos, ypos, zpos, dx, dy, dz)
+# ax.set_xticklabels(countries)
+# ax.set_yticklabels(years)
 
-cmap = cm.get_cmap('jet') # Get desired colormap
+ax.set_xlabel( 'Row' )
+ax.set_ylabel( 'Column' )
+ax.set_zlabel( r'Normalized % change: $|\delta(min, max) \ / \ min|$' )
+
+plt.show()
+
+# ======================================
+# --- 3D BAR COLORMAP PLOTS (B MATRIX)
+# ======================================
+
+delta_B = np.zeros_like( B_nom )
+for ith_Row, ith_Col in np.ndindex(B_nom.shape):
+    print( f'(ith_Row, ith_Col) = ({ith_Row}, {ith_Col})' )
+    max_elem = B_max[ith_Row, ith_Col]
+    min_elem = B_min[ith_Row, ith_Col]
+    delta_B[ith_Row, ith_Col] = get_change(max_elem, min_elem)
+
+# Normalize values
+B_final = delta_B / np.amax(delta_B)
+
+fig = plt.figure()
+ax = plt.axes(projection = "3d")
+
+data = B_final.T
+ 
+numOfRows = data.shape[0]
+numOfCols = data.shape[1]
+ 
+xpos = np.arange(0, numOfCols, 1)
+ypos = np.arange(0, numOfRows, 1)
+xpos, ypos = np.meshgrid(xpos + 0.5, ypos + 0.5)
+ 
+xpos = xpos.flatten()
+ypos = ypos.flatten()
+zpos = np.zeros(numOfCols * numOfRows)
+ 
+dx = np.ones(numOfRows * numOfCols) * 0.5
+dy = np.ones(numOfCols * numOfRows) * 0.5
+dz = data.flatten()
+
+cmap = colormaps['jet'] # Get desired colormap
 max_height = np.max(dz)   # get range of colorbars
 min_height = np.min(dz)
 
 # scale each z to [0,1], and get their rgb values
-rgba = [cmap((k-min_height)/max_height) for k in dz] 
-
+rgba = [cmap((k-min_height)/max_height) for k in dz]
+ax.set_xticks( [i+1 for i in range(0, numOfCols)],
+               [ '$x_1$', '$x_2$', '$x_3$', '$x_4$',
+                 '$x_5$', '$x_6$', '$x_7$', '$x_8$',
+                 '$x_9$', '$x_{10}$', '$x_{11}$' ] )
+ax.set_yticks( [i+1 for i in range(0, numOfRows)], 
+               [ '$u_1$', '$u_2$', '$u_3$', '$u_4$' ] )
 ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=rgba, zsort='average')
+
+ax.set_xlabel( 'Row' )
+ax.set_ylabel( 'Column' )
+ax.set_zlabel( r'Normalized % change: $|\delta(min, max) \ / \ min|$' )
 
 plt.show()
 
