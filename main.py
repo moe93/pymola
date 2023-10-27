f14b49846c437cbb37c84c9e2ea37400aa6543e6
Moe Odeh <m.odeh93@gmail.com>
2023-10-27 13:08:46 -0400

    Ugly but working 3D bar plot based on mean values

--- main.py
+++ main.py
@@ -55,11 +55,18 @@ B_max = B_3D.max( axis=0 ); B_min = B_3D.min( axis=0 )  # min/max across depth
 C_max = C_3D.max( axis=0 ); C_min = C_3D.min( axis=0 )  # min/max across depth
 D_max = D_3D.max( axis=0 ); D_min = D_3D.min( axis=0 )  # min/max across depth
 
+# Get average of matrices
+A_mean = A_3D.mean(axis=0)
+B_mean = B_3D.mean(axis=0)
+C_mean = C_3D.mean(axis=0)
+D_mean = D_3D.mean(axis=0)
+
 # ======================================
 # --- 3D BAR COLORMAP PLOTS (A MATRIX)
 # ======================================
 import  matplotlib.pyplot               as      plt
 from    matplotlib                      import  colormaps # Import colormaps!
+plt.ion()
 
 # Find percent change between max and min
 def get_change( max_elem, min_elem):
@@ -187,6 +194,66 @@ ax.set_ylabel( 'Column' )
 ax.set_zlabel( r'Normalized % change: $|\delta(min, max) \ / \ min|$' )
 
 plt.show()
+
+# ======================================
+# --- 3D BAR COLORMAP PLOTS (A MATRIX) for each azimuth
+# ======================================
+
+for ndx, angle in enumerate( azimuth ):
+    # Get A at angle
+    A_angle = B_3D[ ndx ]
+
+    # Get change from average
+    A_temp = np.zeros_like( B_mean )
+    for ith_Row, ith_Col in np.ndindex(B_mean.shape):
+        crnt_elem = A_angle[ith_Row, ith_Col]
+        mean_elem = A_mean[ith_Row, ith_Col]
+        A_temp[ith_Row, ith_Col] = get_change(crnt_elem, mean_elem)
+
+    # Normalize values
+    AA = A_temp / np.amax(A_temp)
+
+    fig = plt.figure(num=f'Azimuth = {angle}')
+    ax = plt.axes(projection = "3d")
+
+    data = AA.T
+    
+    numOfRows = data.shape[0]
+    numOfCols = data.shape[1]
+    
+    xpos = np.arange(0, numOfCols, 1)
+    ypos = np.arange(0, numOfRows, 1)
+    xpos, ypos = np.meshgrid(xpos + 0.5, ypos + 0.5)
+    
+    xpos = xpos.flatten()
+    ypos = ypos.flatten()
+    zpos = np.zeros(numOfCols * numOfRows)
+    
+    dx = np.ones(numOfRows * numOfCols) * 0.5
+    dy = np.ones(numOfCols * numOfRows) * 0.5
+    dz = data.flatten()
+
+    cmap = colormaps['jet'] # Get desired colormap
+    max_height = np.max(dz)   # get range of colorbars
+    min_height = np.min(dz)
+
+    # scale each z to [0,1], and get their rgb values
+    rgba = [cmap((k-min_height)/max_height) for k in dz]
+    ax.set_xticks( [i+1 for i in range(0, numOfCols)],
+                [ '$x_1$', '$x_2$', '$x_3$', '$x_4$',
+                    '$x_5$', '$x_6$', '$x_7$', '$x_8$',
+                    '$x_9$', '$x_{10}$', '$x_{11}$' ] )
+    # ax.set_yticks( [i+1 for i in range(0, numOfRows)],
+    #            [ '$x_1$', '$x_2$', '$x_3$', '$x_4$',
+    #              '$x_5$', '$x_6$', '$x_7$', '$x_8$',
+    #              '$x_9$', '$x_{10}$', '$x_{11}$' ] )
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=rgba, zsort='average')
+
+    ax.set_xlabel( 'Row' )
+    ax.set_ylabel( 'Column' )
+    ax.set_zlabel( r'Normalized % change: $|\delta(min, max) \ / \ min|$' )
+
+    plt.show()
 
 pass
 
---
+++ pymola/src/test/mean3D.py
@@ -0,0 +1,18 @@
+import  numpy                           as      np
+
+N_DEPTH = 3
+N_ROWS  = 3
+N_COLS  = 3
+
+A = np.zeros( (N_DEPTH, N_ROWS, N_COLS), dtype=np.float32 )
+
+A1 = np.ones( (N_ROWS, N_COLS) ) * 1; A1[0,0] = 10; A1[1,0] = -10
+A2 = np.ones( (N_ROWS, N_COLS) ) * 2; A2[1,1] = 20; A2[2,1] = -20
+A3 = np.ones( (N_ROWS, N_COLS) ) * 3; A3[2,2] = 30; A3[0,2] = -30
+
+A[0] = A1; A[1] = A2; A[2] = A3
+
+A_min = A.min(axis=0); A_max = A.max(axis=0); A_mean = A.mean(axis=0)
+
+
+pass
